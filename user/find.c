619f8c067a96ea3f83400de838df9a7abc370747
Arwa <arwamahmood27@outlook.com>
2026-09-15 12:15:15 +0500

    Assignment 1 (q1 and 2)
    > uptime.c (created)
    > find.c (updated) -- functions from grep.c for regex

--- user/find.c
+++ user/find.c
@@ -36,6 +36,51 @@ run_exec(char *file)
   }
 }
 
+//------------regex matchher copied from grep.c --------------------
+int matchhere(char *, char *);
+int matchstar(int, char *, char *);
+
+
+int
+match(char *re, char *text)
+{
+  if (re[0] == '^')
+    return matchhere(re + 1, text);
+  do { // must look at empty string
+    if (matchhere(re, text))
+      return 1;
+  } while (*text++ != '\0');
+  return 0;
+}
+
+// matchhere: search for re at beginning of text
+int
+matchhere(char *re, char *text)
+{
+  if (re[0] == '\0')
+    return 1;
+  if (re[1] == '*')
+    return matchstar(re[0], re + 2, text);
+  if (re[0] == '$' && re[1] == '\0')
+    return *text == '\0';
+  if (*text != '\0' && (re[0] == '.' || re[0] == *text))
+    return matchhere(re + 1, text + 1);
+  return 0;
+}
+
+// matchstar: search for c*re at beginning of text
+int
+matchstar(int c, char *re, char *text)
+{
+  do { // a * matches zero or more instances
+    if (matchhere(re, text))
+      return 1;
+  } while (*text != '\0' && (*text++ == c || c == '.'));
+  return 0;
+}
+
+// -------------------REGX MATCHER  ENDS----------------------------
+
 void
 find(char *path, char *target)
 {
@@ -56,8 +101,7 @@ find(char *path, char *target)
   }
 
   if (st.type == T_FILE) {
-      printf("%s\n", path);
-    if (strcmp(fmtname(path), target) == 0){
+    if (match(target, fmtname(path))) {
 	if (exec_argc > 0)
 	    run_exec(path);
 	else
